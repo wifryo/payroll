bc34869053995462a019e6d9aab9ed67927d359c
William <wifryo@gmail.com>
2023-05-23 20:53:39 +0200

    Continue implementation

---
+++ src/main/java/wifryo/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package wifryo.payroll;
+
+
+class EmployeeNotFoundException extends RuntimeException {
+
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
--- src/test/java/wifryo/payroll/PayrollApplicationTests.java
+++ src/test/java/wifryo/payroll/PayrollApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package wifryo.payroll;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
